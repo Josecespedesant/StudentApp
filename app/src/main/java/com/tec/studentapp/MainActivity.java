8b0f996e7ee7e392746f455cfa26af719c709f5d
dnsing <33098069+dnsing@users.noreply.github.com>
2018-11-17 19:08:39 -0600

    Log in Facebook v.2

--- app/src/main/java/com/tec/studentapp/MainActivity.java
+++ app/src/main/java/com/tec/studentapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -30,6 +31,7 @@ import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends AppCompatActivity {
+    TextView carnet;
     private static final String TAG = "MainActivity";
     private CallbackManager callbackManager;
     private LoginButton loginButton;
@@ -39,13 +41,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.tec.studentapp",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(getApplicationContext(),"ak7",Toast.LENGTH_SHORT).show();
-                goTestSesion();
+                if(carnet.getText().length() == 10){
+                    Toast.makeText(getApplicationContext(),"ak7",Toast.LENGTH_SHORT).show();
+                    Intent intentt = new Intent(MainActivity.this, RegistrationActivity.class);
+                    startActivity(intentt);
+                    //goTestSesion();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Scanear carnet primero",Toast.LENGTH_LONG).show();
+
+                }
+
             }
             @Override
             public void onCancel() {
@@ -57,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"CANELADO PA",Toast.LENGTH_SHORT).show();
             }
         });
+
 
         try {
 
@@ -74,13 +101,24 @@ public class MainActivity extends AppCompatActivity {
         } catch (NoSuchAlgorithmException e) {
             Log.d(TAG, e.getMessage(), e);
         }
-        final EditText nombreusuariologin = (EditText) findViewById(R.id.nombreusuariologin);
-        final EditText contraseñalogin = (EditText) findViewById(R.id.passwordlogin);
-        final TextView registrate = (TextView) findViewById(R.id.registratetext);
-        final ImageView linkedin = (ImageView) findViewById(R.id.linkedimg);
-        final RelativeLayout iniciarsesion = (RelativeLayout) findViewById(R.id.iniciarsesion);
+        //final EditText nombreusuariologin = (EditText) findViewById(R.id.nombreusuariologin);
+      //  final EditText contraseñalogin = (EditText) findViewById(R.id.passwordlogin);
+      //  final TextView registrate = (TextView) findViewById(R.id.registratetext);
+       //final ImageView linkedin = (ImageView) findViewById(R.id.linkedimg);
+     //   final RelativeLayout iniciarsesion = (RelativeLayout) findViewById(R.id.iniciarsesion);
+        carnet = (TextView) findViewById(R.id.carnet);
+        carnet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, BarcodeScanner.class);
+                startActivityForResult(registerIntent, 1);
 
-        registrate.setOnClickListener(new View.OnClickListener() {
+                //RegistrationActivity.this.startActivity(registerIntent);
+            }
+        });
+
+
+     /*   registrate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(MainActivity.this, RegistrationActivity.class);
@@ -88,17 +126,24 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-        });
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        });*/
     }
 
     private void goTestSesion(){
         Intent intent = new Intent(this, TestSesion.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                String resultado = data.getStringExtra("resultado");
+                carnet.setText(resultado);
+            }
+        }
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/tec/studentapp/TestSesion.java
+++ app/src/main/java/com/tec/studentapp/TestSesion.java
@@ -4,6 +4,7 @@ import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 
@@ -16,8 +17,14 @@ public class TestSesion extends AppCompatActivity {
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
-        if(accessToken == null){
-            goLoginin();
+        TextView carnet = (TextView) findViewById(R.id.carnet);
+
+        if(accessToken == null ){
+            Intent intentt = new Intent(TestSesion.this, RegistrationActivity.class);
+            startActivity(intentt);
+            //goLoginin();
+        }else{
+
         }
     }
 
